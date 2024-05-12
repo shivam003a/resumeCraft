d92eb7e3279f374b4d4eebcfda469c2ff7b54ca5
Shivam Singh <shivam003a@gmail.com>
2024-05-12 16:20:34 +0530

    vercel analytics added

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -6,11 +6,17 @@ import { HashRouter } from 'react-router-dom'
 import { Toaster } from 'react-hot-toast'
 import { Provider } from 'react-redux'
 import { store } from './redux/store.js'
+// Only for vercel analytics, remove if not required
+import { SpeedInsights } from "@vercel/speed-insights/next"
+import { Analytics } from "@vercel/analytics/react"
 
 ReactDOM.createRoot(document.getElementById('root')).render(
     <Provider store={store}>
         <HashRouter>
             <App />
+            // Only for vercel analytics, remove if not required
+            <SpeedInsights />
+            <Analytics />
             <Toaster position="bottom-right" />
         </HashRouter>
     </Provider>,
