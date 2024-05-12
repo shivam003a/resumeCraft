38c5d84269b1919d4ba6ed822ceb93ac501eb471
Shivam Singh <shivam003a@gmail.com>
2024-05-12 16:29:34 +0530

    removed speed insight

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -7,7 +7,7 @@ import { Toaster } from 'react-hot-toast'
 import { Provider } from 'react-redux'
 import { store } from './redux/store.js'
 // Only for vercel analytics, remove if not required
-import { SpeedInsights } from "@vercel/speed-insights/next"
+import { SpeedInsights } from "@vercel/speed-insights"
 import { Analytics } from "@vercel/analytics/react"
 
 ReactDOM.createRoot(document.getElementById('root')).render(
